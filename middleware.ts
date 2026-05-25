153148488279823871b82de51df6aa2f95b85762
undone0603 <undone.k@gmail.com>
2026-05-25 09:52:01 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { NextResponse, type NextRequest } from 'next/server';
-import { middleware as supabaseMiddleware } from './utils/supabase/middleware';
-import { middleware as brandMiddleware } from './server/_core/brand-middleware';
 
-export async function middleware(req: NextRequest) {
-  const res = await supabaseMiddleware(req);
-  const brandRes = brandMiddleware(req);
-  brandRes.headers.forEach((v, k) => res.headers.set(k, v));
-  return res;
+export function middleware(req: NextRequest) {
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: '/:path*',
+};
