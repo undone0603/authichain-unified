6d2f95b7b52d472cb6414abb875672af5eb3d659
undone0603 <undone.k@gmail.com>
2026-08-25 21:44:26 -0400

    Update sdk.ts

--- server/_core/sdk.ts
+++ server/_core/sdk.ts
@@ -2,6 +2,7 @@ import { AXIOS_TIMEOUT_MS, COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
 import { ForbiddenError } from "@shared/_core/errors";
 import axios, { type AxiosInstance } from "axios";
 import cookie from "cookie"; // FIXED
+const parseCookieHeader = cookie.parse;
 import type { Request } from "express";
 import { SignJWT, jwtVerify } from "jose";
 import type { User } from "../../drizzle/schema";
